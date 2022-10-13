f0b37f8f1680a64ca631c62ae5e09206e4476f16
BenBktech <bbrucher@gmail.com>
2022-10-13 15:41:26 +0200

    Updated deploy

--- BackEnd/deploy/00-deploy-NFT.js
+++ BackEnd/deploy/00-deploy-NFT.js
@@ -1,4 +1,3 @@
-
 const { network, ethers } = require("hardhat");
 const { developmentChains, networkConfig } = require("../helper-hardhat-config")
 const { verify } = require("../utils/verify");
@@ -9,17 +8,20 @@ module.exports = async function({ getNamedAccounts, deployments }) {
     const { deployer } = await getNamedAccounts();
 
     const args = [];
+    log("///1///")
     const NFTIsERC721A = await deploy("NFTIsERC721A", {
         from: deployer,
         args: args,
         log: true,
-        waitConfirmations: network.config.blockConformations,
+        waitConfirmations: 6,
     })
+    log("///2///")
 
     if(developmentChains.includes(network.name)) {
         const NFTIsERC721A = await ethers.getContract(
             "NFTIsERC721A"
         );
+        log("///3///")
     }
 
     if(!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
